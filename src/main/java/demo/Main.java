db1d06d174fc05b02f3bb6a4ca8572eceafe2a63
kongaman <christian.kranz@gmail.com>
2019-02-02 16:43:07 +0100

    added hibernate code to save book

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,5 +1,10 @@
 package demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,6 +13,14 @@ public class Main {
 		book.setBookName("Der weisse Hai");
 		book.setBookName("dunno");
 		
+		Configuration config = new Configuration();
+		SessionFactory sessionFactory = config.configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(book);
+		tx.commit();
+		session.close();
+		sessionFactory.close();
 	}
 
 }
